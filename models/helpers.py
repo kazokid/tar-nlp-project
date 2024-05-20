2fd7d2f99ba3f9971595fad48276900adca86822
kazokid <lovro.kovacic7@gmail.com>
2024-05-21 01:44:06 +0200

    started chi square test, added subtaks arg to helper

--- models/helpers.py
+++ models/helpers.py
@@ -6,13 +6,14 @@ CLASSES_SUBTASK_3_PATH = os.path.join(
 )
 
 
-def get_articles_folder_path(language, type):
+def get_articles_folder_path(language, type, subtask):
     """Function that constructs the path to the folder given language and type (train, dev, test)
 
     Args:
     ------
         language (str): Language of the dataset (en, es, fr, ge, gr, it, ka, po, ru)
         type (str): Type of the dataset (train, dev, test)
+        subtask (int): Subtask of the dataset (default is 3)
 
     Returns:
     --------
@@ -20,29 +21,33 @@ def get_articles_folder_path(language, type):
     """
 
     return os.path.join(
-        BASE_PATH, f"bundle/data/{language}/{type}-articles-subtask-3/"
+        BASE_PATH, f"bundle/data/{language}/{type}-articles-subtask-{subtask}/"
     )
 
 
-def get_paths(language, model_name="default_model"):
+def get_paths(language, model_name="default_model", subtask=3):
     """Function that constructs the train and dev paths for the given language
 
     Args:
     ------
         language (str): Language of the dataset (en, es, fr, ge, gr, it, ka, po, ru)
+        model_name (str): Name of the model
+        subtask (int): Subtask of the dataset (default is 3)
 
     Returns:
     --------
         paths (dict): Dictionary with paths to the train folder, dev folder, train labels file and dev labels file
     """
     paths = {
-        "train_folder": get_articles_folder_path(language, "train"),
-        "dev_folder": get_articles_folder_path(language, "dev"),
+        "train_folder": get_articles_folder_path(language, "train", subtask),
+        "dev_folder": get_articles_folder_path(language, "dev", subtask),
         "train_labels": os.path.join(
-            BASE_PATH, f"bundle/data/{language}/train-labels-subtask-3.txt"
+            BASE_PATH,
+            f"bundle/data/{language}/train-labels-subtask-{subtask}.txt",
         ),
         "dev_labels": os.path.join(
-            BASE_PATH, f"bundle/data/{language}/dev-labels-subtask-3.txt"
+            BASE_PATH,
+            f"bundle/data/{language}/dev-labels-subtask-{subtask}.txt",
         ),
         "train_predictions": os.path.join(
             BASE_PATH, f"outputs/{model_name}/{language}-train-predictions.txt"
